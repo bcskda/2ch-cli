734ed5106f6b6451cbc7c71a448e5a4becfc8ea4
bcskda <demulkup@ya.ru>
2016-09-04 21:30:42 +0300

    New test in main()

--- src/2ch-cli.c
+++ src/2ch-cli.c
@@ -7,8 +7,26 @@
 // ========================================
 
 int main (void) {
-  getBoardCatalogJSON ("b",true);
-  printf ("\n\n\n");
-  getThreadJSON ("abu",42375,true);
+  //getBoardCatalogJSON ("b",true);
+  //printf ("\n\n\n");
+  char* thread = getThreadJSON ("abu",42375,true);
+  int postcount = 0;
+  int* posts = findPostsInJSON (thread, &postcount);
+  for (int i = 0; i < postcount-1; i+=1) {
+    printf ("%2d ] ", i);
+    int tmpsize = posts[i+1]-posts[i]-1;
+    char* tmp = (char*) calloc (sizeof(char),tmpsize);
+    tmp = memcpy (tmp, thread+posts[i], tmpsize*sizeof(char));
+    printf ("%s\n",tmp);
+    free (tmp);
+  }
+  printf ("%2d ] ", postcount-1);
+  int tmpsize = strlen(thread)-posts[postcount-1]-1;
+  char* tmp = (char*) calloc (sizeof(char),tmpsize);
+  tmp = memcpy (tmp, thread+posts[postcount-1], tmpsize*sizeof(char));
+  printf ("%s\n",tmp);
+  free (tmp);
+  free (posts);
+  free (thread);
   return 0;
 }
