c15307010bfb207ba7f19d8f123bf4d4b8e8190d
bcskda <demulkup@ya.ru>
2016-12-17 17:59:42 +0300

    1. Проверка длины аргументов! 2. Возвращаемые значения main() 3. Комментарии 4. Tab-отступы

--- src/2ch-cli.c
+++ src/2ch-cli.c
@@ -6,62 +6,68 @@
 
 #include "2ch-cli.h"
 
-void pomogite() //помощь
+void pomogite() // Справка
 {
-    printf("2ch-cli "VERSION" - консольный клиент двача\n");
-    printf("Использование:\n");
-    printf(" -h - помощь\n");
-    printf(" -s - запуск\n");
-    printf(" -p - задать пасскод\n");
-    printf(" -b - задать борду\n");
-    printf(" -n - заранее задать номер поста\n");
+	printf("2ch-cli "VERSION" - консольный клиент двача\n");
+	printf("Использование:\n");
+	printf(" -h - помощь\n");
+	printf(" -s - запуск\n");
+	printf(" -p - задать пасскод\n");
+	printf(" -b - задать борду\n");
+	printf(" -n - заранее задать номер поста\n");
 }
 
 int main (int argc, char **argv)
 {
-    //всё это по идее должно быть в хедере, но почему-то там оно не работает
-    //для двачей переменные
-    char passcode[64] = "пасскода нет нихуя :("; //храним пасскод
-    char board_name[10] = "b"; //имя нужной борды
-    long long int post_number = 0; //номер поста в борде
+	char passcode[64] = "пасскода нет нихуя :("; // Пасскод
+	char board_name[10] = "b"; // Имя борды
+	long long int post_number = 0; // Номер треда в борде
 
 	//getopt
-    int opt;
-    bool start_or_not = false; //понадобится при 's', чтобы после была проверка запускать или нет.
+	int opt;
+	bool start_or_not = false; // Проверяем, есть ли '-s' и нужно ли запускать
 
-    if (argc == 1) /* Если аргументов нет, вывод помощи */ {
-        pomogite(); //если анон запустил прогу без аргументов, то пишет помощь.
-        return -1;
-    }
-    while (( opt = getopt(argc, argv, "hp:b:n:s") ) != -1) //пока то что в скобках не равно -1
+	if (argc == 1) /* Если аргументов нет, вывод помощи */ {
+		pomogite();
+		return RET_NOARGS;
+	}
+	while (( opt = getopt(argc, argv, "hp:b:n:s") ) != -1)
 	{
 		switch (opt)
 		{
 			case 'p':
-                memset(passcode, '\0', sizeof(passcode)); //Почистим наш буфер
-                strcat(passcode, optarg);
+				memset(passcode, '\0', sizeof(passcode));
+				if ( sizeof(optarg) > sizeof(passcode) ) {
+					printf("Не шути так больше\n");
+					return ERR_ARGS;
+				}
+				memcpy(passcode, optarg, sizeof(passcode));
 				printf("Разраб хуй, ещё не запилил\n");
-                printf("Уже что-то могу: %s!\n", passcode);
-                break;
-            case 'b':
-                memset(board_name, '\0', sizeof(board_name));
-                strcpy(board_name, optarg);
-                break;
-            case 'n':
-                post_number = atoi(optarg);
-                break;
-            case 's':
-                start_or_not = true;
-                break;
+				printf("Уже что-то могу: %s!\n", passcode);
+				break;
+			case 'b':
+				memset(board_name, '\0', sizeof(board_name));
+				if ( sizeof(optarg) > sizeof(board_name) ) {
+					printf("Не шути так больше\n");
+					return ERR_ARGS;
+				}
+				memcpy(board_name, optarg, sizeof(board_name));
+				break;
+			case 'n':
+				post_number = atoi(optarg);
+				break;
+			case 's':
+				start_or_not = true;
+				break;
 			default:
 				pomogite();
-				return 0;
+				return ERR_ARGS;
 		}
 	}
 
-    if (start_or_not == false) return 0; //проверка запускать или нет
+	if (start_or_not == false) return 0;
 
-    setlocale (LC_ALL, "");
+	setlocale (LC_ALL, "");
 
 	makabaSetup();
 	
@@ -78,28 +84,29 @@ int main (int argc, char **argv)
 	}
 	thread_ch = memcpy(
 					thread_ch,
-                    getThreadJSON (board_name, post_number, false),
+					getThreadJSON (board_name, post_number, false),
 					Thread_size
 					);
-    struct thread* thread = initThread(thread_ch, sizeof(thread), true);
+	struct thread* thread = initThread(thread_ch, sizeof(thread), true);
 
 	initscr();
 	raw();
 	keypad (stdscr, TRUE);
 	noecho();
-    printw ("Push [c] to clear screen, [q] to exit, anything else to print another post\n");
+	printw ("Push [c] to clear screen, [q] to exit, anything else to print another post\n");
 	for (int i = 0; i < thread->nposts; i++) {
 		bool done = 0;
 		while (! done)
-			switch (getch()) { 
+			switch (getch())
+			{ 
 				case 'C': case 'c':
 					clear();
-                    printw ("Push [c] to clear screen, [q] to exit, anything else to print another post\n");
+					printw ("Push [c] to clear screen, [q] to exit, anything else to print another post\n");
 					break;
-                case 'Q': case 'q':
-                    done = 1;
-                    i = thread->nposts;
-                    break;
+				case 'Q': case 'q':
+					done = 1;
+					i = thread->nposts;
+					break;
 				default:
 					fprintf(stderr, "Printing post #%d\n", i);
 					printPost(thread->posts[i], true, true);
@@ -125,7 +132,8 @@ int printPost (struct post* post,const bool show_email,const bool show_files) {
 	if (show_email && (post->email != NULL)) {
 		printw ("[=== %s (%s) #%d %s ===]\n%s\n\n",
 			post->name, post->email, post->num, post->date, post->comment->text);
-	} else {
+	}
+	else {
 		printw ("[=== %s #%d %s ===]\n%s\n\n",
 			post->name, post->num, post->date, post->comment->text);
 	}
