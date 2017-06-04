42e355728968664689686b554dcb574c64b18347
bcskda <demulkup@ya.ru>
2017-06-04 21:38:26 +0300

    Кэш связан с Makaba

--- src/makaba.h
+++ src/makaba.h
@@ -8,11 +8,27 @@
 #include <string>
 #include <vector>
 #include <json/json.h>
+#include <cstdio>
+#include <cstddef>
+#include <memory>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
 #include "API.h"
 #include "parser.h"
 #include "external.h"
 #include "error.h"
 #pragma once
+
+
+extern const size_t Json_cache_buf_size;
+extern const char *Json_cache_suff_armed;
+extern char *Json_cache_buf;
+extern char Json_cache_dir[100];
+
+
+extern const char *CaptchaPngFilename;
+extern const char *CaptchaUtfFilename;
 
 
 namespace Makaba {
@@ -82,3 +98,12 @@ namespace Makaba {
 	};
 
 }
+
+
+int   initJsonCache  ();
+bool  checkJsonCache (const Makaba::Thread &thread);
+void  armJsonCache   (const Makaba::Thread &thread);
+void  disarmJsonCache(const Makaba::Thread &thread);
+char *readJsonCache  (const Makaba::Thread &thread, long long *threadsize);
+int   writeJsonCache (const Makaba::Thread &thread, const char *thread_ch);
+int   cleanJsonCache ();
--- src/parser.h
+++ src/parser.h
@@ -4,37 +4,17 @@
 // (Headers)
 // ========================================
 
-#pragma once
-
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
 #include <string>
-#include <cstddef>
-#include <memory>
-#include <sys/stat.h>
-#include <dirent.h>
-#include <errno.h>
 #include "error.h"
 #include "external.h"
-
-// ========================================
-// JSON cache
-// ========================================
-
-extern const size_t Json_cache_buf_size;
-extern const char *Json_cache_suff_armed;
-extern char *Json_cache_buf;
-extern char Json_cache_dir[100];
-
-// ========================================
-// Ручной парсинг
-// ========================================
+#pragma once
 
 extern const char *PATTERN_TAG_OPEN;
 extern const char *PATTERN_TAG_CLOSE;
-
 extern const char *PATTERN_HREF_OPEN;
 extern const char *PATTERN_HREF_CLOSE;
 extern const char *PATTERN_REPLY_CLASS;
@@ -50,19 +30,6 @@ extern const char *PATTERN_SINGLE_QUOT;
 extern const char *PATTERN_DOUBLE_QUOT;
 extern const char *PATTERN_AMP;
 
-extern const char *CaptchaPngFilename;
-extern const char *CaptchaUtfFilename;
-
-// ========================================
-// Прототипы
-// ========================================
 
 char *parseHTML(const char *raw, const long long raw_len, const bool v);
 
-int   initJsonCache  ();
-bool  checkJsonCache (const Makaba::Thread &thread);
-void  armJsonCache   (const Makaba::Thread &thread);
-void  disarmJsonCache(const Makaba::Thread &thread);
-char *readJsonCache  (const Makaba::Thread &thread, long long *threadsize);
-int   writeJsonCache (const Makaba::Thread &thread, const char *thread_ch);
-int   cleanJsonCache ();
